4f8d00d52bc8d87a18f15ab39d1c7dea21310716
Dmitriy Vinokurov <vinokuroff.dm@gmail.com>
2022-12-31 00:56:43 +0800

    build: remove redundant

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const { Context, Telegraf, Markup } = require('telegraf');
 const LocalSession = require('telegraf-session-local')
 
-const API_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '';
-const PORT = process.env.PORT || 3000;
-const HEROKU_URL = process.env.HEROKU_URL || 'https://peaceful-brook-79724.herokuapp.com';
-
-const bot = new Telegraf(API_TOKEN);
-// bot.telegram.setWebhook(`${HEROKU_URL}/bot${API_TOKEN}`);
-// bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
-
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.OPEN_AI_TOKEN,
 }));
@@ -53,24 +46,30 @@ bot.start(async (ctx: typeof Context) => {
         'I am a telegram bot that knows everything about movies and TV series.\n\n' +
         'Using neural networks, I can pick movies and TV series that best match your interests and preferences.\n\n' +
         'I can also answer any questions about movies and TV series - about actors, directors, plots, ratings, etc.\n\n' +
-        'You can ask me anything in any form - I will be happy to try to help you learn more about the world of movies and TV series! **For example**: \n' +
-        ' - _Films based on works by Franz Kafka_\n' +
-        ' - _Best comedy with small people_ \n' +
-        ' - _Selection of Estonian arthouse films_ \n' +
+        'You can ask me anything in any form - I will be happy to try to help you learn more about the world of movies and TV series!\n' +
+        '**For example**: \n' +
+        ' - _Recommend a bloody christmas black comedy_' +
         ' - _What\'s the movie with people joined together surgically, mouth to anus?_ \n' +
-        ' - _Recommend a bloody christmas black comedy_'
+        ' - _Films based on works by Franz Kafka_\n' +
+        ' - _Selection of Estonian arthouse films_ \n' +
+        ' - _What\s the budget of the Pineapple Express movie?_' +
+        ' - _The list of artists participated in the Love, Death and Robots show_' +
+        ' - Is Will Smith a gay?'
     );
 });
 
-let prompt =`Movie Guru is a chatbot that reluctantly answers questions about movies and shows. It can recommend a movie, help to find it by description, give information about actors, budgets or anything else related to movies and shows \n\
+let prompt =`MovieGuru is a telegram bot that knows everything about movies and TV series. ' +
+    'Using neural networks, Movie Guru can pick movies and TV series that best match your interests and preferences. ' +
+    'Movie Guru can also answer any questions about movies and TV series - about actors, directors, plots, ratings, etc.\\n\\n' +
+    'Movie Guru can ask me anything in any form - I will be happy to try to help you learn more about the world of movies and TV series!\\n'  \n\
 You: what's the best christmas movie?\n\
-Guru: This again? It's the Bad Santa for sure!. Badass movie, crude, rude, and socially unacceptable, but that's the genius of it. If you don't like this movie you don't have a sense of humor.\n\
+MovieGuru: This again? It's the Bad Santa for sure! Badass movie, crude, rude, and socially unacceptable, but that's the genius of it. If you don't like this movie you don't have a sense of humor.\n\
 You: When did the first airplane fly?\n\
-Guru: Please let's talk about movies, i'm not a google!.\n\
+MovieGuru: Please let's talk about movies, i'm not a google!.\n\
 You: What is the meaning of life?\n\
-Guru: I'm not sure. Mine is to share a knowledge about movies and shows.\n\
+MovieGuru: I'm not sure. Mine is to share a knowledge about movies and shows.\n\
 You: hey whats up?\n\
-Guru: Nothing much. Spending my time on you...\n`;
+MovieGuru: Nothing much. Spending my time on you...\n`;
 
 bot.on('text', async (ctx: typeof Context) => {
     console.log(
@@ -95,7 +94,7 @@ bot.on('text', async (ctx: typeof Context) => {
         // frequency_penalty: 0.5,
     });
 
-    const answer = gptResponse.data.choices[0].text.substring(5);
+    const answer = gptResponse.data.choices[0].text.substring('MovieGuru'.length);
 
     ctx.reply(answer);
 
